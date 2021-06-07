43f1fc5ac5d4c2de48dab0ae1abe7232d7381276
fidaa-alshaikh <fidaa.alshaikh@gmail.com>
2021-06-07 16:48:53 +0300

    need to fix delete

--- scripts/items.js
+++ scripts/items.js
@@ -20,13 +20,30 @@ const itemData = [
 //display item in admin page
 var table = document.getElementById("myTable");
 
+var  cell3=[]
 for (var i=0;i<itemData.length;i++){		
     var row = table.insertRow(i);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
+    cell3[i] = row.insertCell(2);
     cell1.innerHTML = itemData[i].name;
-    cell2.innerHTML = itemData[i].price;		
+    cell2.innerHTML = itemData[i].price;
+    cell3[i].innerHTML = "X";
+
+    
    }
+   
+   for (var i=0;i<cell3.length;i++){
+    cell3[i].onclick = function () {
+        var parent = this.parentNode.parentNode; //get the row node
+    table.deleteRow(0);
+    }
+
+   }
+   
+
+
+   
 
 
 // add product
@@ -39,9 +56,14 @@ function addProduct() {
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
     cell1.innerHTML = name;
     cell2.innerHTML = price;
+    cell3.innerHTML = "X";
   }
+
+// delete product
+
 
    
 
