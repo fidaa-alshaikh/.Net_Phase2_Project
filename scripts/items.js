92ae50c884a7c353bc04ed3022092b9d58f1c8c4
fidaa-alshaikh <fidaa.alshaikh@gmail.com>
2021-06-07 10:01:56 +0300

    fiv the structure

---
+++ scripts/items.js
@@ -0,0 +1,17 @@
+const itemData = [
+    {
+        name: 'Computer',
+        pic: 'JavaScript',
+        price: '5000',
+    },
+    {
+        name: 'iPhone 11',
+        pic: 'JavaScript',
+        price: '3500',
+    },
+    {
+        name: 'Microsoft Mouse',
+        pic: 'JavaScript',
+        price: '350',
+    },
+];
